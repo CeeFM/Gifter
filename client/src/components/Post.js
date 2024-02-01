e54b86eebfa4b45f84905d860271414fc4a3bbe6
Colin Matteson <seefm@proton.me>
2024-01-31 21:15:49 -0500

    tried to do some conditional rendering in post.js to make post comments display below the caption if there were any, but i'm not doing it right at all, so I will wait for tomorrow's review to troubleshoot further

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,6 +4,19 @@ import { Link } from "react-router-dom";
 
 
 export const Post = ({ post }) => {
+
+    function returnComments() {
+      let html = "";
+      if (post.Comments.length > 0) {
+        for (let i = 0; i < post.Comments.length; i++) {
+          html += `<p><strong>${post?.Comments[i]?.userProfile?.name}<strong> says: ${post?.Comments[i]?.Message}</p><br />`
+        }
+        return html;
+      } else {
+        return
+      }
+    }
+
   return (
     <Card className="m-4">
       <p className="text-left px-2">Posted by: {post.userProfile.name}</p>
@@ -15,6 +28,9 @@ export const Post = ({ post }) => {
           </Link>
         </p>
         <p>{post.caption}</p>
+        <p>
+          {returnComments}
+        </p>
       </CardBody>
     </Card>
   );
